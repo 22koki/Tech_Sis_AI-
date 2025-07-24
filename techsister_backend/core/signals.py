edbfed693526a5bb5bebdb261876b20cd4d4839c
fay <wahomeimani@gmail.com>
2025-07-24 18:13:51 +0300

    updated signals.py

--- techsister_backend/core/signals.py
+++ techsister_backend/core/signals.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import UserProfile
 
+# Create profile when new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+# Save profile when user is saved
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()

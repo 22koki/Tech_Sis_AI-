4a9bdb9c38e384c8a23207b6250311a96f639145
fay <wahomeimani@gmail.com>
2025-07-24 18:14:42 +0300

    updated apps.py

--- techsister_backend/core/apps.py
+++ techsister_backend/core/apps.py
@@ -6,3 +6,11 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         import core.signals
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        import core.signals  # Make sure this path matches your actual app name
